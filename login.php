335ee7e172f9b01b7c3b4bf82f991256a160f405
HidanParra <s.piopietrelchina@gmail.com>
2019-07-16 21:34:11 -0500

    Se agregan los php y se personalizan las paginas

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Dark Bootstrap Admin by Bootstrapious.com</title>
+    <title>Sistemita | Iniciar Sesion</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
@@ -35,37 +35,27 @@
               <div class="info d-flex align-items-center">
                 <div class="content">
                   <div class="logo">
-                    <h1>Dashboard</h1>
+                    <h1>Accesa al Area Administrable</h1>
                   </div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  <p></p>
                 </div>
               </div>
             </div>
             <!-- Form Panel    -->
-            <div class="col-lg-6 bg-white">
+            <div class="col-lg-6">
               <div class="form d-flex align-items-center">
                 <div class="content">
-                  <form class="text-left form-validate">
-                    <div class="form-group-material">
-                      <input id="register-username" type="text" name="registerUsername" required data-msg="Please enter your username" class="input-material">
-                      <label for="register-username" class="label-material">Username</label>
+                  <form method="get" class="form-validate mb-4">
+                    <div class="form-group">
+                      <input id="login-username" type="text" name="loginUsername" required data-msg="Please enter your username" class="input-material">
+                      <label for="login-username" class="label-material">Usuario</label>
                     </div>
-                    <div class="form-group-material">
-                      <input id="register-email" type="email" name="registerEmail" required data-msg="Please enter a valid email address" class="input-material">
-                      <label for="register-email" class="label-material">Email Address      </label>
+                    <div class="form-group">
+                      <input id="login-password" type="password" name="loginPassword" required data-msg="Please enter your password" class="input-material">
+                      <label for="login-password" class="label-material">Contrasena</label>
                     </div>
-                    <div class="form-group-material">
-                      <input id="register-password" type="password" name="registerPassword" required data-msg="Please enter your password" class="input-material">
-                      <label for="register-password" class="label-material">Password        </label>
-                    </div>
-                    <div class="form-group terms-conditions text-center">
-                      <input id="register-agree" name="registerAgree" type="checkbox" required value="1" data-msg="Your agreement is required" class="checkbox-template">
-                      <label for="register-agree">I agree with the terms and policy</label>
-                    </div>
-                    <div class="form-group text-center">
-                      <input id="register" type="submit" value="Register" class="btn btn-primary">
-                    </div>
-                  </form><small>Already have an account? </small><a href="login.html" class="signup">Login</a>
+                    <button type="submit" class="btn btn-primary">Inicia Sesion</button>
+                  </form><a href="#" class="forgot-pass">¿Olvidaste tu contraseña?</a><br><small>¿No tienes cuenta? </small><a href="register.php" class="signup">Registrate</a>
                 </div>
               </div>
             </div>
@@ -73,7 +63,7 @@
         </div>
       </div>
       <div class="copyrights text-center">
-        <p>Design by <a href="https://bootstrapious.com" class="external">Bootstrapious</a></p>
+        <p>Design by <a href="https://bootstrapious.com/p/bootstrap-4-dark-admin" class="external">Bootstrapious</a></p>
         <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
       </div>
     </div>
